acadd84108151ad1f623278632379806f155cd88
ftcteam8513 <ftcteam8513@gmail.com>
2016-10-01 19:32:24 -0400

    Created new op mode Removed Extra This is A POV drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EstesETeleopPOV.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EstesETeleopPOV.java
@@ -50,7 +50,7 @@ import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
  * It also opens and closes the claws slowly using the left and right Bumper buttons.
  *
  * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list.
  */
 
 @TeleOp(name="Pushbot: Teleop POV", group="Pushbot")
